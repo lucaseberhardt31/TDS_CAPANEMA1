7372f8e3dc473716563fce7fa6fae812ab9d11bd
lucaseberhardt31 <lucas.eberhardt31@gmail.com>
2026-05-04 21:54:31 -0300

    operadores em JS

---
+++ operadores/desafioOperadores.js
@@ -0,0 +1,12 @@
+//desafio
+
+/*  
+atalho: shift + alt + a
+01. utilizando os operadores relacionados declare 2 variáveis e imprima no output/console 5 valores true 5 valores false
+ */  
+
+let numeroDeDedos = 5
+let meiaDuzia = 6
+console.log(numeroDeDedos > meiaDuzia)
+console.log(meiaDuzia < numeroDeDedos)
+console.log(numeroDeDedos + meiaDuzia !== 11)
---
+++ operadores/unario.js
@@ -0,0 +1,10 @@
+let a = 1; // 1
+a = a + 1 // 2
+a += 1; // 3
+a++; // 4 unário
+console.log(a);
+
+a = a - 1; // 3
+a -= 1; // 2
+a--; // 1 unário
+console.log(a);
