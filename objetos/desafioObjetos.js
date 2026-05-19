adf2ca0d3c0afab898da5fd68103c68171ee1f9e
lucaseberhardt31 <lucas.eberhardt31@gmail.com>
2026-05-18 21:51:00 -0300

    update do desafio de objeto

--- objetos/desafioObjetos.js
+++ objetos/desafioObjetos.js
@@ -3,6 +3,7 @@ Desafio: Criar um objato q contenha informações de um filme favorito,
 sendo necessário inserir uma nova propriedade, modificar uma existente
 e excluir uma propriedade. No final imprimir o objeto no console.
 */
-const melhorFilmeDeTodosOsTempos = [
-    {}
+const melhoresFilmesDeTodosOsTempos = [
+    {nome: "Shrek1", prota: "Burro", antagonista: "anão"},
+    {nome: "Gato de Botas 2", prota:}
 ]
